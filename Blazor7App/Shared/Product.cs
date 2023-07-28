2e3f007c2c47d2a7dbc35c547caf5b1778e8b15f
Derek Peacock <derek.peacock@yahoo.com>
2023-07-28 20:01:36 +0100

    Added Product model

---
+++ Blazor7App/Shared/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blazor7App.Shared
+{
+    public enum ProductCategories
+    {
+        None,
+        Electronics,
+        Groceries,
+        Home,
+        Toys,
+        Clothing
+    }
+
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        public string Name { get; set; } = "";
+
+        public string Description { get; set; } = "";
+
+        public ProductCategories Category { get; set; } = ProductCategories.None;
+
+        public string Image { get; set; } = "";
+
+        public decimal Price { get; set; } = 0;
+    }
+}
